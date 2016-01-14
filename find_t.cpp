c947cff022d1e331b93ec6c459eacec6709f9950
bug-wang <wsw1990wsw@gmail.com>
2016-01-14 13:06:37 +0800

    Update find_t.cpp

--- find_t.cpp
+++ find_t.cpp
@@ -8,26 +8,46 @@
 #define MAX 1000
 using namespace std;
 
-//int find_t(int a[][],int m,int n,int t)
-//{
-	
-//}
+void find_t(int a[MAX][MAX],int m,int n,int t)
+{		
+	vector<int> v;
+	for(int i=0;i<m;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			v.push_back(a[i][j]);
+		}
+	}
+	int i=0;
+	for(;i<v.size();i++)
+	{
+		if(t==v[i])	
+		{
+			cout<<"yes"<<endl;
+			break;
+		}	
+	}
+	if(i==v.size())
+	{
+		cout<<"no"<<endl;
+	}
+}
 
 int main(void)
 {
 	int m,n,t;
 	//static int a[MAX][MAX];
-	
+	cin>>m>>n;
+	cin>>t;
 	int  (*p)[MAX] = new int[MAX][MAX]; 
 	
-	for(int i=0;i<1000;i++)
+	for(int i=0;i<m;i++)
 	{
-		for(int j=0;j<1000;j++)
+		for(int j=0;j<n;j++)
 		{
-			p[i][j]=0;
+			cin>>p[i][j];
 		}
 	}
-	//cout<<m<<n;
-
 	
+	find_t(p,m,n,t);
 }

93f3f9f49bdd774b35ca8655636f25c501943d84
bug-wang <wsw1990wsw@gmail.com>
2016-01-14 10:33:14 +0800

    Create find_t.cpp

---
+++ find_t.cpp
@@ -0,0 +1,33 @@
+//http://ac.jobdu.com/problem.php?pid=1384
+//这个题目遇到了栈溢出的问题，可以看我博客http://www.cnblogs.com/wswang/p/5127754.html
+//至于如何查找就两种方法，这里没有具体写（懒
+//还是要靠实践的，不然不知道会出什么问题，大家可以直接遍历查找，不过这样效率低
+//也可以先查找在某行范围内，然后遍历这一行，想了想还是补充上吧
+
+#include"header_file.h"
+#define MAX 1000
+using namespace std;
+
+//int find_t(int a[][],int m,int n,int t)
+//{
+	
+//}
+
+int main(void)
+{
+	int m,n,t;
+	//static int a[MAX][MAX];
+	
+	int  (*p)[MAX] = new int[MAX][MAX]; 
+	
+	for(int i=0;i<1000;i++)
+	{
+		for(int j=0;j<1000;j++)
+		{
+			p[i][j]=0;
+		}
+	}
+	//cout<<m<<n;
+
+	
+}

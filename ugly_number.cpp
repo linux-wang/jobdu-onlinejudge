f2081127d7b8f177d9652977cfae5769ee4dbb6e
bug-wang <wsw1990wsw@gmail.com>
2016-02-29 15:12:42 +0800

    update ugly_number.cpp

--- ugly_number.cpp
+++ ugly_number.cpp
@@ -1,3 +1,12 @@
+/*
+题目链接： http://ac.jobdu.com/problem.php?pid=1214
+首先判断是否是丑数：开始我的想法是找因子，然后判断是不是只有这三个因子
+不过想了想如果是29这种素数，还得判断是不是2，3，太麻烦，然后看了一下网上
+见： http://www.zybang.com/question/ae859a42ba22e27173be66a934d6ce3f.html
+思路就明了了，后来自己又推断，也走到了上面的思路，见链接
+ 
+*/
+
 #include<vector>
 #include<iostream>
 using namespace std;
@@ -10,32 +19,32 @@ int ugly_number(int n)
 	}
 	else
 	{
-		vector<int> v;
-		for(int i=2;i<=n;i++)
+		while(n%2==0)
 		{
-			if(n%i==0)
-			{
-				v.push_back(i);
-			}
-		}
-		int i=0;
-		for(;i<v.size();)
+			n=n/2;
+		} 
+		while(n%3==0)
 		{
-			if(i==2||i==3||i==5)
-			{
-				i++;
-			}
-			else
-			{
-				return 0;
-				break;
-			}//break;
-			
+			n=n/3;
 		}
-		if(i==v.size())
+		while(n%5==0)
+		{
+			n=n/5;
+		}
+		while(n%7==0)
+		{
+			n=n/7;
+		}
+		if(n==0)
 		{
 			return 1;
 		}
+		if(n==1)
+		{
+			return 1;
+		}
+		else
+			return 0;
 		
 	}
 }
@@ -44,6 +53,9 @@ int main(void)
 {
 	int n;
 	cin>>n;
-	
-	cout<<ugly_number(n)<<endl;
+	vector<int> v;
+	for(int i=1;i<100;i++)
+		if(ugly_number(i))
+			v.push_back(i);
+	cout<<v[n-1]<<endl;
 }
